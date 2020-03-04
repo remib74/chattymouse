b2afc640b8b6fa9ef687c8b71b157dcd3d667b16
remib74 <33649152+remib74@users.noreply.github.com>
2020-03-04 06:16:26 +0100

    insertin fonction smiley

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <!-- dès ouverture de la page écriture du cookie utilisateur avec un $i a hasard-->            
 <?php 
             if(empty($_COOKIE['userOnline'])):
-            $id=rand(200,30000030);
+            $id=rand(200,3000000);
             setcookie('userOnline','User'.$id,time()+3600);  
         endif;
         ?>
@@ -20,6 +20,7 @@
 <script src="js/refresh.js" type="text/javascript"></script>
 <script src="js/verif.js" type="text/javascript"></script>
 <script src="js/jquery-3.4.1.min.js" type="text/javascript"></script>
+<script src="js/replaceSmiley.js"></script>
 <link href="css/reset.css" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet">
 
@@ -54,7 +55,33 @@
             </main>
         <div id="msgConsole">
             <div id="emoji">
-            <p>Emojis</p>
+            <?php $smileys=[[
+            ':)'=>'img/001.gif',
+            ':(('=>'img/002.gif',
+            ':('=>'img/003.gif',
+            '%|'=>'img/004.gif',
+            '00°'=>'img/005.gif',
+            '^^)'=>'img/006.gif',
+            '++0'=>'img/007.gif',
+            '8)'=>'img/008.gif',
+            ':D)'=>'img/009.gif',
+            ':0'=>'img/0010.gif',
+            ':¨¨'=>'img/011.gif',
+            '..°°'=>'img/012.gif']
+            ]; ?>
+            <p>Emojis :<?php 
+            
+            
+            for($i=1;$i<=12;$i++):
+                $imgSrc="img/0$i.gif";
+                if($i>=10){
+                    echo "<div><a href='#'><img src ='img/$i.gif'></a></div>"; 
+                }
+                else{
+                
+                echo "<div><a href='#' onclick='replaceSmiley(11);'><img src ='$imgSrc'></a></div>"; 
+                }
+            endfor;?></p>
             </div>
             <div id="console">
             <!--le formulaire pour saisir des messages le champ nickname est en lecture seule-->
@@ -62,7 +89,7 @@
                 <div id="frmwriteNck">
                 <input type="text" id="nickname" maxlength="15" name="nickname" class="frminput" value="<?php if(isset($_COOKIE['userOnline'])):echo($_COOKIE['userOnline']);endif;?>" readonly>
                 </div>
-                <input type="textarea" rows="5" cols="50" minlength="1" maxlength="300" id="zonetxt" name="zonetxt" class="frminput" value=""></textarea>
+                <input type="textarea" rows="5" cols="50" minlength="1" maxlength="3img/00" id="zonetxt" name="zonetxt" class="frminput" value=""></textarea>
                 <input type="submit" name="sendit" id="sendit" value="send" onclick="verif(frmwrite);">
                 <input type="reset" name="resetit" id="resetit" value="reset form">
                 </form>
---
+++ js/replaceSmiley.js
@@ -0,0 +1,5 @@
+function replaceSmiley(smile){
+alert (smile);
+//const smileRep=document.getElementById("zonetxt").value;
+//const smileRep=smileRep+smile;
+}
